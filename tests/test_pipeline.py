7aa194241b490b85e9625b2c5ff9dafffaae1428
ynouri <ynouri@users.noreply.github.com>
2019-12-01 22:22:29 -0500

    Add pubsub env variables as globals in GH actions

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -3,6 +3,7 @@ from scrapy_pubsub import PubSubItemPipeline
 from tests.settings import PUBSUB_PROJECT_ID, PUBSUB_TOPIC
 
 
+# TODO: write unit tests by mocking Pub/Sub publisher
 def test_object():
     """Test that the class can be instantiated."""
     obj = PubSubItemPipeline(PUBSUB_PROJECT_ID, PUBSUB_TOPIC)
